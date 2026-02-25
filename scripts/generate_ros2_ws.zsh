58436e95419c1b7859b5a4db72550a3038677c23
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2026-02-25 14:43:53 +0100

    Script to create a ROS 2 workspace

---
+++ scripts/generate_ros2_ws.zsh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+# This script will run from the parent folder
+# It will create a ROS 2 workspace inside the project folder (PARENT_PATH)
+#   - with the name provided as the first argument, or 'new_ros2_ws' if no argument is given.
+# 
+# It can also create a ROS2 project if the workspace name is an absolute path
+
+
+# Look for the parent directory of this script
+# which should be the root of the project 
+PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)/..
+cd "$PARENT_PATH"
+
+# Assign the first argument ($1) to ws_name.
+# If $1 is empty, use 'new_ros2_ws' as the default.
+ws_name=${1:-new_ros2_ws}
+
+# If the workspace exists, exit
+[[ -d "$ws_name/src" ]] && print "ROS 2 workspace $ws_name already exists. Skipping creation." && exit 0
+
+# If the workspace doesn't exist, create it
+print "Creating ROS 2 workspace: $ws_name"    
+mkdir -p "$ws_name/src"
+
+# Initialize the workspace with colcon
+print "Building ROS 2 workspace"
+cd $ws_name
+colcon build
+
+

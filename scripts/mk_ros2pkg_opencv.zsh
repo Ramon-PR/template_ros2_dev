83a47eb9df82a311b0e05e7d60d6736103488f72
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2026-02-25 14:45:05 +0100

    Script to add a ROS2 package with OpenCV dependencies to the ROS2 workspace

---
+++ scripts/mk_ros2pkg_opencv.zsh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+
+# This script will run from the parent folder
+# If a ROS2 workspace doesn't exist, it will create a ROS 2 workspace inside the project folder (PARENT_PATH)
+#   - with the name provided as the first argument, or 'new_ros2_ws' if no argument is given.
+# It will then create a ROS 2 package inside the ROS2 workspace
+#   - with the name provided as the second argument, or default 'pkg_ros2_opencv'
+#   - This package has the dependencies needed to work with OpenCV in ROS2, 
+#     and to create a ROS2 node that can subscribe to image topics and process them with OpenCV.
+# 
+# It can also create a ROS2 project if the workspace name is an absolute path
+
+
+# Look for the parent directory of this script
+# which should be the root of the project 
+PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)/..
+cd "$PARENT_PATH"
+
+ws_name=${1:-new_ros2_ws}
+pkg_name=${2:-pkg_ros2_opencv}
+
+dependency_list=(
+    "rclpy" # ROS2 Python client library
+    "sensor_msgs" # For handling image messages
+    "std_msgs" # For standard message types
+    "image_transport" # For efficient image transport
+    "cv_bridge" # For converting between ROS images and OpenCV images
+    "python3-opencv" # OpenCV library for Python
+)
+
+
+# If the workspace doesn't exist, create it
+zsh generate_ros2_ws.zsh $ws_name
+
+# Create the ROS 2 package with the specified dependencies
+# ros2 pkg create --build-type ament_python $pkg_name --dependencies ${dependency_list[@]}
+cd "$ws_name/src"
+
+if [[ ! -d "$pkg_name" ]]; then
+    print "Package $pkg_name does not exist. Creating..."
+    
+    ros2 pkg create --build-type ament_python "$pkg_name" \
+        --dependencies "${dependency_list[@]}"
+    
+    [[ $? -eq 0 ]] && print "Package $pkg_name created successfully."
+else
+    print "Package $pkg_name already exists."
+fi
+
+
